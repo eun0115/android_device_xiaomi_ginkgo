fbf5138f0e1695bdb2256cc544b9f5ad8d094e4d
Jiakai Zhang <jiakaiz@google.com>
2022-05-20 14:40:27 +0100

    ginkgo: Preopt SystemUI for AOSP builds.
    Bug: 232860596
    Test: See no dex2oat invocation on the first boot.
    Change-Id: Icc38efb4b702b518caa22a3abfc7c04d6ba1ac2a
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- device.mk
+++ device.mk
@@ -140,6 +140,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-1.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml
 
+# Dexpreopt
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI
+
 # Doze
 PRODUCT_PACKAGES += \
     XiaomiDoze
